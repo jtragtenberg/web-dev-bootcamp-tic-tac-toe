de93a5aa12dec877f99a41d19fe27909adcd01d7
Joao Tragtenberg <tragtenberg@gmail.com>
2022-01-09 18:07:53 -0300

    styled header and player blocks

---
+++ app.js
@@ -0,0 +1,5 @@
+//Configure Player Names -> Form with input field in modeal overlay; also validate user input and show validation feedback
+//(Re)Start Game -> "Start Game" button should clear current game board or "Game Over" message
+//Turn-based Gameplay -> Turns should switch automatically between the two players, every player has his/her own symbol
+//Select Fields & Check for winner -> Game fields are clickable & the player's symbol is displayed: Check for winner (regular Tic-Tac-Toe rules) after every turn
+//Show "Game Over" Window -> Persent "Game Over" message which highlights winner (or draw) once the game ends.
