b817ecbefeb350827433dbb24913bf3bc4b9eb68
Joao Tragtenberg <tragtenberg@gmail.com>
2022-01-11 19:40:33 -0300

    javascript code for the username configuration

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,22 @@
-//initialize html elements
+let editedPlayer = 0;
+const players = [
+    {
+        name: "",
+        symbol: "X"
+    },
+    {
+        name: "",
+        symbol: "O"
+    }
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay"); //o config.js é chamado antes no HTML, mas esse objeto só é chamado com add event listener deste app.js, então esse objeto deve ser declarado antes do listener
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
-
 const cancelConfigButtonElement = document.getElementById("cancel-config-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig); //openPlayerConfig() defined in config.js
@@ -12,3 +24,5 @@ editPlayer2BtnElement.addEventListener("click", openPlayerConfig); //openPlayerC
 
 cancelConfigButtonElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig); //event from the form element when I press the submit button, but is associated to the whole form
--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,41 @@
-function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = "block";
-    backdropElement.style.display = "block";
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; //se fosse um data-player-id, precisaria colocar["player-id"]; O + é pra transformar o string em um número
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
 }
 
 function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = "none";
-    backdropElement.style.display = "none";
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = ""; //just to practice DOM digging
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault(); //stops the browser to execute the standard submit button behaviour reloading the page sending http requests
+  const formData = new FormData(event.target); //instantiating an object from a class with the new keyword
+  const enteredPlayerName = formData.get("playername").trim();
+  console.log(enteredPlayerName);
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name";
+    return; //prevent the rest of the savePlayerConfig funcion to run
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  //   if (editedPlayer === 1) {
+  //       players[0].name = enteredPlayerName;
+  //   } else {
+  //       players[1].name = enteredPlayerName;
+  //   }
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
