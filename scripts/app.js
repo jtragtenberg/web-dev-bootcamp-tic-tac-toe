324cd5411e25aee1ad95c27dd9e4697ce86fb6c5
Joao Tragtenberg <tragtenberg@gmail.com>
2022-01-12 09:57:41 -0300

    Implemented Logic to start games

--- scripts/app.js
+++ scripts/app.js
@@ -14,10 +14,12 @@ const playerConfigOverlayElement = document.getElementById("config-overlay"); //
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigButtonElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig); //openPlayerConfig() defined in config.js
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig); //openPlayerConfig() defined in config.js
@@ -26,3 +28,5 @@ cancelConfigButtonElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig); //event from the form element when I press the submit button, but is associated to the whole form
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
