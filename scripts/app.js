626c43bc8ffb9aa7fd11c090c5b3decc5f8da2b7
Joao Tragtenberg <tragtenberg@gmail.com>
2022-01-12 10:52:52 -0300

    managing field selection in tthe game, round management

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
 let editedPlayer = 0;
 const players = [
     {
@@ -9,17 +15,21 @@ const players = [
         symbol: "O"
     }
 ];
+let activePlayer = 0;
 
 const playerConfigOverlayElement = document.getElementById("config-overlay"); //o config.js é chamado antes no HTML, mas esse objeto só é chamado com add event listener deste app.js, então esse objeto deve ser declarado antes do listener
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigButtonElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll("#game-board li");
+const gameBoardElement = document.getElementById("game-board");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig); //openPlayerConfig() defined in config.js
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig); //openPlayerConfig() defined in config.js
@@ -30,3 +40,9 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig); //event from the form element when I press the submit button, but is associated to the whole form
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// for(const gameFieldElement of gameFieldElements){
+//     gameFieldElement.addEventListener("click", selectGameField);
+// }
+
+gameBoardElement.addEventListener("click", selectGameField);
--- scripts/game.js
+++ scripts/game.js
@@ -3,5 +3,38 @@ function startNewGame() {
         alert("Please set custom player names for both players!");
         return;
     }
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = "block";
+}
+
+function switchPlayer(){
+    if(activePlayer === 0){
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+    const selectedField = event.target;
+
+    if (selectedField.tagName != "LI"){
+        return;
+    }
+    
+    const selectedColumn = selectedField.dataset.col - 1;
+    const selectedRow = selectedField.dataset.row - 1;
+    
+    if (gameData[selectedRow][selectedColumn] > 0){
+        alert("Please select an empty field");
+        return;
+    }
+    
+    selectedField.textContent = players[activePlayer].symbol; //starts with players[0]
+    selectedField.classList.add("disabled");
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+    switchPlayer();
 }
